0a9e22f5e4e4eb796f9531a764fbce316fc9fde9
Dan Johnson <dsjohns2@illinois.edu>
2018-04-24 16:11:33 -0500

    add available driver node

--- event_functions.py
+++ event_functions.py
@@ -21,29 +21,7 @@ def have_free_driver(sys):
     return False
 
 
-def sched_if_available_driver(sys, rider_id, rider_location):
-    if have_free_driver(sys):
-        min_driver_id = None
-        min_distance = None
-        for driver_id, driver_loc in enumerate(sys.driver_current_locations):
-            curr_distance = distance(rider_location, driver_loc)
-            if sys.driver_is_free[driver_id] and \
-                    (min_distance is None
-                     or curr_distance < min_distance):
-                min_driver_id = driver_id
-                min_distance = curr_distance
-        sys.driver_is_free[min_driver_id] = False
-        print("Driver {} is taking request from rider {}"
-              .format(min_driver_id, rider_id))
 
-        new_event_time = sys.cur_time + 15
-        # FIXME: Not sure if this is supposed to be higher or lower priority
-        new_event = event.event(
-            "Driver Response", new_event_time, node_driver_response,
-            args={'rider_id': rider_id, 'driver_id': min_driver_id},
-            priority=4)
-        eventlist_tuple = (new_event_time, new_event)
-        heapq.heappush(sys.eventlist, eventlist_tuple)
 
 # These are the nodes in the event graph.
 
@@ -84,10 +62,41 @@ def node_rider_request(sys, args):
         "Rider Request", new_event_time, node_rider_request)
     eventlist_tuple = (new_event_time, new_event)
     heapq.heappush(sys.eventlist, eventlist_tuple)
-
-    sched_if_available_driver(sys, rider_id, rider_location)
+	
+	new_event_time = sys.cur_time
+	new_event = event.event("Available Driver", new_event_time, node_available_driver, args={'rider_id': rider_id, 'rider_location': rider_location})
+	eventlist_tuple = (new_event_time, new_event)
+	heapq.heappush(sys.eventlist, eventlist_tuple)
 
     print("")
+
+def node_available_driver(sys, rider_id, rider_location):
+	rider_id = args["rider_id"]
+	rider_location = args["rider_location"]
+    if have_free_driver(sys):
+		# State changes
+        min_driver_id = None
+        min_distance = None
+        for driver_id, driver_loc in enumerate(sys.driver_current_locations):
+            curr_distance = distance(rider_location, driver_loc)
+            if sys.driver_is_free[driver_id] and \
+                    (min_distance is None
+                     or curr_distance < min_distance):
+                min_driver_id = driver_id
+                min_distance = curr_distance
+    	sys.driver_is_free[min_driver_id] = False
+        print("Driver {} is taking request from rider {}"
+              .format(min_driver_id, rider_id))
+
+		# Schedule events
+        new_event_time = sys.cur_time + 15
+        # FIXME: Not sure if this is supposed to be higher or lower priority
+        new_event = event.event(
+            "Driver Response", new_event_time, node_driver_response,
+            args={'rider_id': rider_id, 'driver_id': min_driver_id},
+            priority=4)
+        eventlist_tuple = (new_event_time, new_event)
+        heapq.heappush(sys.eventlist, eventlist_tuple)
 
 
 def node_driver_response(sys, args):
@@ -96,6 +105,9 @@ def node_driver_response(sys, args):
     print("")
 
     # State changes
+    driver_id = args['driver_id']
+    rider_id = args['rider_id']
+    sys.driver_current_locations[driver_id] = sys.rider_start_location[rider_id]
 
     # Schedule events
     new_event_time = sys.cur_time + 20
@@ -121,5 +133,8 @@ def node_end_of_drive(sys, args):
         sys.rider_end_times[rider_id] - sys.rider_start_times[rider_id]))
 
     # Schedule events
-    sched_if_available_driver(sys, rider_id, rider_location)
+	new_event_time = sys.cur_time
+	new_event = event.event("Available Driver", new_event_time, node_available_driver, args={'rider_id': rider_id, 'rider_location': rider_location})
+    eventlist_tuple = (new_event_time, new_event)
+    heapq.heappush(sys.eventlist, eventlist_tuple)
     print("")
