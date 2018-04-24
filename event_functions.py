349705363903565d0e89064e03d6cf2b7596df3d
Terry <terry.h@outlook.com>
2018-04-24 16:15:00 -0500

    Fixed indents

--- event_functions.py
+++ event_functions.py
@@ -21,8 +21,6 @@ def have_free_driver(sys):
     return False
 
 
-
-
 # These are the nodes in the event graph.
 
 
@@ -62,19 +60,21 @@ def node_rider_request(sys, args):
         "Rider Request", new_event_time, node_rider_request)
     eventlist_tuple = (new_event_time, new_event)
     heapq.heappush(sys.eventlist, eventlist_tuple)
-	
-	new_event_time = sys.cur_time
-	new_event = event.event("Available Driver", new_event_time, node_available_driver, args={'rider_id': rider_id, 'rider_location': rider_location})
-	eventlist_tuple = (new_event_time, new_event)
-	heapq.heappush(sys.eventlist, eventlist_tuple)
+
+    new_event_time = sys.cur_time
+    new_event = event.event("Available Driver", new_event_time, node_available_driver, args={
+                            'rider_id': rider_id, 'rider_location': rider_location})
+    eventlist_tuple = (new_event_time, new_event)
+    heapq.heappush(sys.eventlist, eventlist_tuple)
 
     print("")
 
-def node_available_driver(sys, rider_id, rider_location):
-	rider_id = args["rider_id"]
-	rider_location = args["rider_location"]
+
+def node_available_driver(sys, args):
+    rider_id = args["rider_id"]
+    rider_location = args["rider_location"]
     if have_free_driver(sys):
-		# State changes
+        # State changes
         min_driver_id = None
         min_distance = None
         for driver_id, driver_loc in enumerate(sys.driver_current_locations):
@@ -84,11 +84,11 @@ def node_available_driver(sys, rider_id, rider_location):
                      or curr_distance < min_distance):
                 min_driver_id = driver_id
                 min_distance = curr_distance
-    	sys.driver_is_free[min_driver_id] = False
+        sys.driver_is_free[min_driver_id] = False
         print("Driver {} is taking request from rider {}"
               .format(min_driver_id, rider_id))
 
-		# Schedule events
+        # Schedule events
         new_event_time = sys.cur_time + 15
         # FIXME: Not sure if this is supposed to be higher or lower priority
         new_event = event.event(
@@ -107,7 +107,7 @@ def node_driver_response(sys, args):
     # State changes
     driver_id = args['driver_id']
     rider_id = args['rider_id']
-    sys.driver_current_locations[driver_id] = sys.rider_start_location[rider_id]
+    sys.driver_current_locations[driver_id] = sys.rider_start_locations[rider_id]
 
     # Schedule events
     new_event_time = sys.cur_time + 20
@@ -133,8 +133,9 @@ def node_end_of_drive(sys, args):
         sys.rider_end_times[rider_id] - sys.rider_start_times[rider_id]))
 
     # Schedule events
-	new_event_time = sys.cur_time
-	new_event = event.event("Available Driver", new_event_time, node_available_driver, args={'rider_id': rider_id, 'rider_location': rider_location})
+    new_event_time = sys.cur_time
+    new_event = event.event("Available Driver", new_event_time, node_available_driver, args={
+                            'rider_id': rider_id, 'rider_location': rider_location})
     eventlist_tuple = (new_event_time, new_event)
     heapq.heappush(sys.eventlist, eventlist_tuple)
     print("")
